78e3f3d7cc9285738e0ac6642e8fd5bd33cab9f1
iiitiansaurabh <79242161+genius-0963@users.noreply.github.com>
2025-08-30 05:17:44 +0530

    Remove all folders from repository, keep only README.md and .gitignore

--- Downloads/Leetcode-Premium-Unlocker-master/raiseit-platform-main/server/routes/officer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getOfficerDetails, updateOfficerDetails } = require('../controllers/officer')
-// const { roleAuthenticationMiddleware } = require('../middleware/roleAuthentication')
-
-router.route('/').get(getOfficerDetails).patch(updateOfficerDetails)
-
-module.exports = router                                                        
--- Downloads/Leetcode-Premium-Unlocker-master/raiseit-platform-main/server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getUserDetails, updateUserDetails } = require('../controllers/user')
-// const { roleAuthenticationMiddleware } = require('../middleware/roleAuthentication')
-
-router.route('/').get(getUserDetails).patch(updateUserDetails)
-
-module.exports = router
